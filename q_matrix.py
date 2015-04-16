72c259466cd5cc7e8b9b29799be9b13807e6cd69
Galarius <galarius.is@gmail.com>
2015-04-16 19:14:40 +0300

    Working generalized Q-matrix.

--- q_matrix.py
+++ q_matrix.py
@@ -11,14 +11,14 @@ class QMatrix:
         self.p = p
         self.n = n
         self.Q = []
-        for i in range(0, self.p+1):
+        for i in range(0, p+1):
             mm = []
-            for j in range(0, self.p+1):
-                if self.p == 1:
-                    n = self.n - self.p + j if i != 0 and i != self.p else (self.n + 1 - j if i != self.p else self.n - j)
+            for j in range(0, p+1):
+                if p == 1:
+                    k = n - p + j if i != 0 and i != p else (n + 1 - j if i != p else n - j)
                 else:
-                    n = self.n - self.p + j if i != 0 and i != self.p else (self.n + 1 - j if i != self.p else self.n - j)
-                mm.append(self.fib(n))
+                    k = n + 1 - j if i == 0 else n - (p-1) + (i-1) - j
+                mm.append(self.fib(k))
             self.Q.append(mm)
 
     def pretty_print(self):
@@ -33,26 +33,22 @@ class QMatrix:
         print ''
 
     def det(self):
-        return (-1)**(self.p*self.slotActivationDatan)
+        return (-1)**(self.p*self.n)
 
     def fib(self, n):
         """
         Fibonacci extended to negative range.
-
-        ToDo: optimize (memorization, direct calculation)
         """
-        # return (-1)**(abs(n)+1)*self.fib(abs(n)) if n < -self.p else (0 if n < 0 else (self.fib(n-1) +
-        #                          self.fib(n-self.p-1) if n > self.p+1 else (0 if n == 0 else 1)))
         if n == 0:
             return 0
         elif n > self.p+1:
             return self.fib(n-1) + self.fib(n-self.p-1)
         elif n > 0 or n == -self.p:
             return 1
-        elif n > -self.p:
+        elif n >= -self.p+1:
             return 0
         else:
-            return (-1)**(abs(n)+1)*self.fib(abs(n))
+            return self.fib(n+self.p+1) - self.fib(n+self.p)
 
 
     @staticmethod
@@ -76,48 +72,51 @@ class QMatrix:
         return M
 
 
-q = QMatrix(2, 2)
-# q.pretty_print()
-#q = QMatrix(2, -1)
-#q.pretty_print()
-# M = message_to_matrix("kkjljkl")
-# QMatrix.pretty_print_matrix(M)
-# E = QMatrix.encode_matrix_message(M, 1)
-# M = QMatrix.decode_matrix_message(E, 1)
-# QMatrix.pretty_print_matrix(E)
-# QMatrix.pretty_print_matrix(M)
-
-# test
-
-q = QMatrix(1, 0)
-print [q.fib(x) for x in range(-8, 9)]
-q = QMatrix(2, 0)
-print [q.fib(x) for x in range(-8, 9)]
-q = QMatrix(3, 0)
-print [q.fib(x) for x in range(-8, 9)]
-q = QMatrix(4, 0)
-print [q.fib(x) for x in range(-8, 9)]
-q = QMatrix(5, 0)
-print [q.fib(x) for x in range(-8, 9)]
-
-q = QMatrix(2, 2)
-print q.fib(3), q.fib(2), q.fib(1)
-print q.fib(1), q.fib(0), q.fib(-1)
-print q.fib(2), q.fib(1), q.fib(0)
-print ""
+q = QMatrix(2, 1)
+q.pretty_print()
 q = QMatrix(2, -1)
-print q.fib(0), q.fib(-1), q.fib(-2)
-print q.fib(-2), q.fib(-3), q.fib(-4)
-print q.fib(-1), q.fib(-2), q.fib(-3)
-print ""
-q = QMatrix(3, 3)
-print q.fib(4), q.fib(3), q.fib(2), q.fib(1)
-print q.fib(1), q.fib(0), q.fib(-1), q.fib(-2)
-print q.fib(2), q.fib(1), q.fib(0), q.fib(-1)
-print q.fib(3), q.fib(2), q.fib(1), q.fib(0)
-print ""
-q = QMatrix(3, -1)
-print q.fib(0), q.fib(-1), q.fib(-2), q.fib(-3)
-print q.fib(-3), q.fib(-4), q.fib(-5), q.fib(-6)
-print q.fib(-2), q.fib(-3), q.fib(-4), q.fib(-5)
-print q.fib(-1), q.fib(-2), q.fib(-3), q.fib(-4)
+q.pretty_print()
+M = message_to_matrix("kkjljkl")
+QMatrix.pretty_print_matrix(M)
+E = QMatrix.encode_matrix_message(M, 1)
+M = QMatrix.decode_matrix_message(E, 1)
+QMatrix.pretty_print_matrix(E)
+QMatrix.pretty_print_matrix(M)
+
+# test 1
+# q = QMatrix(1, 0)
+# print [q.fib(x) for x in range(-8, 9)][::-1]
+# q = QMatrix(2, 0)
+# print [q.fib(x) for x in range(-8, 9)][::-1]
+# q = QMatrix(3, 0)
+# print [q.fib(x) for x in range(-8, 9)][::-1]
+# q = QMatrix(4, 0)
+# print [q.fib(x) for x in range(-8, 9)][::-1]
+# q = QMatrix(5, 0)
+# print [q.fib(x) for x in range(-8, 9)][::-1]
+# test 2
+# q = QMatrix(2, 2)
+# q.pretty_print()
+# print q.fib(3), q.fib(2), q.fib(1)
+# print q.fib(1), q.fib(0), q.fib(-1)
+# print q.fib(2), q.fib(1), q.fib(0)
+# print ""
+# q = QMatrix(2, -1)
+# q.pretty_print()
+# print q.fib(0), q.fib(-1), q.fib(-2)
+# print q.fib(-2), q.fib(-3), q.fib(-4)
+# print q.fib(-1), q.fib(-2), q.fib(-3)
+# print ""
+# q = QMatrix(3, 3)
+# q.pretty_print()
+# print q.fib(4), q.fib(3), q.fib(2), q.fib(1)
+# print q.fib(1), q.fib(0), q.fib(-1), q.fib(-2)
+# print q.fib(2), q.fib(1), q.fib(0), q.fib(-1)
+# print q.fib(3), q.fib(2), q.fib(1), q.fib(0)
+# print ""
+# q = QMatrix(3, -1)
+# q.pretty_print()
+# print q.fib(0), q.fib(-1), q.fib(-2), q.fib(-3)
+# print q.fib(-3), q.fib(-4), q.fib(-5), q.fib(-6)
+# print q.fib(-2), q.fib(-3), q.fib(-4), q.fib(-5)
+# print q.fib(-1), q.fib(-2), q.fib(-3), q.fib(-4)
